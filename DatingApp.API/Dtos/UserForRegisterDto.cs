5ed6b7b40ec85297097a3024dbf7b97e08df1e9d
Sandrine <scarmooch@gmail.com>
2020-05-18 20:40:40 +0200

    End of Section 3

---
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(16, MinimumLength=8, ErrorMessage="Password must be between 8 and 16 characters")]
+        public string Password { get; set; }
+    }
+}
